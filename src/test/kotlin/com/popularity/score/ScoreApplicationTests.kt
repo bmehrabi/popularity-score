8674ab8cfabef8cbc0d1fafd9726fba2c3762fe2
Babak Mehrabi <b.mehrabi.91@gmail.com>
2026-01-19 08:28:19 +0100

    Initilizing project with Kotlin and Spring Boot

---
+++ src/test/kotlin/com/popularity/score/ScoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.popularity.score
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
